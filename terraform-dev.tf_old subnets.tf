3f4855cbd889f43557e3cc119948d6b86fa29995
pnaveenkp <naveenkumar123243@gmail.com>
2023-04-18 00:12:28 +0530

    final modifications

---
+++ terraform-dev.tf_old subnets.tf
@@ -0,0 +1,34 @@
+resource "aws_subnet" "subnet1-public" {
+  vpc_id            = aws_vpc.testvpc01.id
+  cidr_block        = var.public_subnet1_cidr
+  availability_zone = "us-east-2a"
+
+
+  tags = {
+    Name = "${var.vpc_name}-subnet-1"
+  }
+}
+
+
+resource "aws_subnet" "subnet2-public" {
+  vpc_id            = aws_vpc.testvpc01.id
+  cidr_block        = var.public_subnet2_cidr
+  availability_zone = "us-east-2b"
+
+
+  tags = {
+    Name = "${var.vpc_name}-subnet-2"
+  }
+}
+
+
+resource "aws_subnet" "subnet3-public" {
+  vpc_id            = aws_vpc.testvpc01.id
+  cidr_block        = var.public_subnet3_cidr
+  availability_zone = "us-east-2c"
+
+
+  tags = {
+    Name = "${var.vpc_name}-subnet-3"
+  }
+}
